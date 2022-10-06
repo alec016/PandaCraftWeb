a2ee892402b5636b5390717dc7332c712d2f1f11
alec016 <78179790+alec016@users.noreply.github.com>
2022-10-07 01:38:35 +0200

    creacion de formulario añadir staff para rating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,  Route, BrowserRouter as Router } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Privacy from './components/Privacy';
+import Profile from './components/Profile';
+import Staff from './components/Staff'
+import StaffAccount from './components/StaffAccount'
+import './components/css/commons.scss'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/PandaCraftWeb/docs/" element={<Home/>}/>
+          <Route path="/PandaCraftWeb/docs/about" element={<About/>}/>
+          <Route path="/PandaCraftWeb/docs/privacy" element={<Privacy/>}/>
+          <Route path="/PandaCraftWeb/docs/profile" element={<Profile/>}/>
+          <Route path="/PandaCraftWeb/docs/profile/staff" element={<Staff/>}/>
+          <Route path="/PandaCraftWeb/docs/profile/staff/account" element={<StaffAccount/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
-
-export default App;
